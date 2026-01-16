7fdba2b573c1a75cd604245122dced8c436cc694
z0fans <z0fans@noreply.github.com>
2026-01-17 01:37:10 +0800

    feat: use port 17897 to avoid clash with local Clash Verge, use full MultiDesk XML template

--- backend/core/config_gen.py
+++ backend/core/config_gen.py
@@ -2,14 +2,17 @@ from pathlib import Path
 import yaml
 
 
+SOCKS_PORT = 17897
+
+
 class ConfigGenerator:
     def __init__(self):
         self._config_dir = Path(__file__).parent.parent
 
     def generate_clash_config(self, proxies: list) -> Path:
         config = {
-            "port": 7890,
-            "socks-port": 7897,
+            "port": 17890,
+            "socks-port": SOCKS_PORT,
             "allow-lan": False,
             "mode": "rule",
             "proxies": proxies,
@@ -27,19 +30,115 @@ class ConfigGenerator:
         return xml_path
 
     def _build_xml(self, servers: list) -> str:
-        items = []
+        server_entries = []
         for server in servers:
-            items.append(f"""    <Connection>
-      <Name>{server.get("name", "Server")}</Name>
-      <Host>{server.get("host", "")}</Host>
-      <Port>{server.get("port", 3389)}</Port>
-      <ProxyType>Socks5</ProxyType>
-      <ProxyHost>127.0.0.1</ProxyHost>
-      <ProxyPort>7897</ProxyPort>
-    </Connection>""")
-        return f"""<?xml version="1.0" encoding="utf-8"?>
+            name = self._escape_xml(server.get("name", "Server"))
+            host = self._escape_xml(server.get("host", ""))
+            server_entries.append(f"""			<Server>
+				<Name>{name}</Name>
+				<Description/>
+				<Server>{host}</Server>
+				<MacAddress/>
+				<UseVMBus>0</UseVMBus>
+				<EnhancedMode>0</EnhancedMode>
+				<VMId/>
+				<InheritGeneral>0</InheritGeneral>
+				<EnableCredSspSupport>1</EnableCredSspSupport>
+				<InheritProxy>1</InheritProxy>
+				<InheritDisplay>1</InheritDisplay>
+				<RedirectPrinters>0</RedirectPrinters>
+				<RedirectClipboard>1</RedirectClipboard>
+				<RedirectPorts>0</RedirectPorts>
+				<RedirectSmartCards>0</RedirectSmartCards>
+				<RedirectDrives>0</RedirectDrives>
+				<DriveCollection/>
+				<AudioRedirectionMode>0</AudioRedirectionMode>
+				<AudioCaptureRedirectionMode>0</AudioCaptureRedirectionMode>
+				<KeyboardHookMode>1</KeyboardHookMode>
+				<StartProgramOnConnection>0</StartProgramOnConnection>
+				<StartProgram/>
+				<WorkDir/>
+				<PerformanceFlags>384</PerformanceFlags>
+				<BitmapPersistence>1</BitmapPersistence>
+				<AutoReconnect>0</AutoReconnect>
+				<BandwidthDetection>1</BandwidthDetection>
+				<AuthenticationLevel>0</AuthenticationLevel>
+				<GatewayProfileUsageMethod>0</GatewayProfileUsageMethod>
+				<GatewayUsageMethod>1</GatewayUsageMethod>
+				<GatewayHostname/>
+				<GatewayCredsSource>4</GatewayCredsSource>
+				<GatewayUserName/>
+				<GatewayPassword/>
+				<GatewayDomain/>
+				<GatewayUseGeneralCred>1</GatewayUseGeneralCred>
+				<UseClientName>0</UseClientName>
+				<ClientName/>
+				<ProxyType>0</ProxyType>
+				<SocksHostname>127.0.0.1</SocksHostname>
+				<SocksPort>{SOCKS_PORT}</SocksPort>
+				<SocksUserName/>
+				<SocksPassword/>
+				<UserName>Administrator</UserName>
+				<Domain/>
+				<Password/>
+				<RDPPort>3389</RDPPort>
+			</Server>""")
+
+        servers_xml = "\n".join(server_entries) if server_entries else ""
+
+        return f"""<?xml version="1.0" encoding="utf-8" standalone="yes"?>
 <MultiDesk>
-  <Connections>
-{chr(10).join(items)}
-  </Connections>
+	<Servers>
+		<Group>
+			<Properties>
+				<Name>LibrasService</Name>
+				<Description/>
+				<InheritGeneral>0</InheritGeneral>
+				<InheritProxy>0</InheritProxy>
+				<ProxyType>1</ProxyType>
+				<SocksHostname>127.0.0.1</SocksHostname>
+				<SocksPort>{SOCKS_PORT}</SocksPort>
+				<SocksUserName/>
+				<SocksPassword/>
+				<InheritDisplay>0</InheritDisplay>
+				<GroupCollapsed>0</GroupCollapsed>
+				<UserName>Administrator</UserName>
+				<Domain/>
+				<Password/>
+				<RDPPort>3389</RDPPort>
+				<DesktopHeight>0</DesktopHeight>
+				<DesktopWidth>0</DesktopWidth>
+				<ZoomLevel>100</ZoomLevel>
+				<DesktopScaleFactor>0</DesktopScaleFactor>
+				<ColorDepth>24</ColorDepth>
+				<FullScreen>0</FullScreen>
+				<ConnectToServerConsole>0</ConnectToServerConsole>
+				<SmartSizing>1</SmartSizing>
+			</Properties>
+{servers_xml}
+		</Group>
+		<Properties>
+			<Name/>
+			<Description/>
+			<InheritGeneral>1</InheritGeneral>
+			<InheritProxy>1</InheritProxy>
+			<InheritDisplay>1</InheritDisplay>
+			<GroupCollapsed>0</GroupCollapsed>
+		</Properties>
+	</Servers>
+	<Settings/>
+	<ExternalTools>
+		<ExternalTool Title="P&amp;ing" Command="%comspec%" Arguments="/k ping -t %server%" StartPath=""/>
+		<ExternalTool Title="&amp;Query User" Command="%comspec%" Arguments="/k query user /server:%server%" StartPath=""/>
+		<ExternalTool Title="&amp;Query Session" Command="%comspec%" Arguments="/k query session /server:%server%" StartPath=""/>
+	</ExternalTools>
 </MultiDesk>"""
+
+    def _escape_xml(self, text: str) -> str:
+        return (
+            text.replace("&", "&amp;")
+            .replace("<", "&lt;")
+            .replace(">", "&gt;")
+            .replace('"', "&quot;")
+            .replace("'", "&apos;")
+        )
